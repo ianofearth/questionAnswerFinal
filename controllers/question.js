3bb72ed04584b7034281ab29b7d8bcd139669dc5
ian bevel <student@epicodus.com>
2015-06-12 11:06:49 -0700

    allow user to delete a question from the question's page

--- controllers/question.js
+++ controllers/question.js
@@ -8,6 +8,12 @@ QuestionAnswer.QuestionController = Ember.ObjectController.extend({
       this.set('isEditing', false);
       var question = this.get('model');
       question.save();
+    },
+    delete: function() {
+      if(confirm('Are you sure you want to remove this Question?')) {
+        this.get('model').destroyRecord();
+        this.transitionToRoute('questions');
+      }
     }
   }
 });
